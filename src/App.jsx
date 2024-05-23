b08cb862f9bca8b4908ac7e19761795cbf04ea1f
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-05-23 21:14:26 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import style from './App.module.css'
 
 function App() {
   const [value, setValue] = useState('');
-  
+  const [notes, setNotes] = useState([]);
+
   function onChangeValue(event){
     setValue(event.target.value);
   }
-  
-  
 
+  const result = notes.map((note, index)=>{
+    return <input key={index} class={style.input} value={value} onChange={onChangeValue}/>
+  })
+
+  function onClickButton(){
+    setNotes([...notes, value]);
+    setValue('');
+  }
+  
   return (
     <div class={style.wrapper}>
       <h2 class={style.title}>WebDevMax</h2>
-      <button>Добавить список дел</button>
-      <input class={style.input} value={value} onChange={onChangeValue}/>
+      <button class={style.button} onClick={onClickButton}>Добавить список дел</button>
+      {result}
     </div>
   )
 }
